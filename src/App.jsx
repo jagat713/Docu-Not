65760053e58f9300cfcdec59632a395bdc68d640
Jagat <beherajagat713@gmail.com>
2026-01-11 20:55:50 +0530

    Made it more responsive and fixed some bugs

--- src/App.jsx
+++ src/App.jsx
@@ -51,17 +51,17 @@ const editNote = (index) => {
     <div className={`min-h-0.5 items-center text-center justify-between px-6 py-4 w-screen text-white border-gray-700 border-b-2 rounded-b-lg mb-4`}>
       <h1 className='text-2xl italic font-bold '>Docu-Note</h1>
     </div>
-    <div className='grid grid-cols-1 md:grid-cols-2 h-screen min-w-auto items-center gap-2 grid-wrap overflow-hidden'>
-      <div className='text-white bg-gray-950 min-w-1/2  rounded-2xl border-2 h-screen flex flex-col p-4 gap-4 border-gray-700'>
-        <form action="submit" className="flex flex-col gap-3 h-screen">
+    <div className='grid grid-cols-1 md:grid-cols-2 h-screen w-full items-center gap-2 grid-wrap'>
+      <div className='text-white bg-gray-950 w-full  rounded-2xl border-2 h-full flex flex-col p-4 gap-4 border-gray-700'>
+        <form action="submit" className="flex flex-col gap-3 h-full">
           <input type="text" value={title} placeholder="Heading" onChange={Heading} className=" italic font-extrabold text-center border-none outline-none focus:outline-none text-2xl"/>
-          <textarea placeholder="Hi..." value={content} onChange={Content} className=" border-none outline-none ring-0 focus:outline-none focus:ring-0 resize-none text-left text-white min-h-100 "></textarea>
+          <textarea placeholder="Hi..." value={content} onChange={Content} className=" h-[90%] border-none outline-none ring-0 focus:outline-none focus:ring-0 resize-none text-left text-white"></textarea>
           <button className='active:bg-white hover:bg-purple-800 active:text-black border-b-2 border-b-cyan-800 rounded-2xl font-bold italic' onClick={onSubmit}>Save</button>
         </form>
       </div>
-      <div className='text-white bg-gray-950 min-w-1/2 h-screen border-2 rounded-2xl border-gray-700 p-4 flex flex-col gap-4'>
+      <div className='text-white bg-gray-950 w-full h-full border-2 rounded-2xl border-gray-700 p-4 flex flex-col gap-4'>
         <h1 className='text-white text-center italic font-bold'>Your Notes</h1>
-        <div className=' bg-gray-950 grid grid-cols-1 gap-1 p-2 h-screen overflow-y-scroll'>
+        <div className=' bg-gray-950 grid grid-cols-1 gap-1 p-2  overflow-y-scroll'>
           {note.map((n, index) => (
             <div key={index} className='text-left justify-center bg-black border-gray-700 border-2 rounded-2xl text-white p-2 overflow-hidden h-30 grid grid-cols-[80%_20%] gap-2'>
               <div className=' h-25 w-full overflow-hidden grid grid-rows-2 gap-1 p-2 rounded-2xl'>
